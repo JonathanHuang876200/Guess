63d5ed100a75092b5dc1c5b167dd76aebfd8c403
JonathanHuang876200 <jonathan876200@gmail.com>
2017-11-08 15:17:26 +0800

    Toast version1

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Random ran = new Random();
+    int r = ran.nextInt(9)+1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,24 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         bSend.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-//                int random = (int) (Math.random() * 10 + 1);
-                int random = 5;
-                if (R.id.ed_number < random) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage("大一點")
-                            .show();
+            public void onClick(View view) { EditText edNumber = (EditText)findViewById(R.id.ed_number);
+                int n = Integer.parseInt(edNumber.getText().toString());
+                if(n<r){
+                    Toast.makeText(MainActivity.this, "大一點", Toast.LENGTH_SHORT).show();
                 }
-                if (R.id.ed_number > random) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage("小一點")
-                            .show();
+                if(n>r){
+                    Toast.makeText(MainActivity.this, "小一點", Toast.LENGTH_SHORT).show();
                 }
-                if (R.id.ed_number == random) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setMessage("猜對了")
-                            .show();
+                if(n==r){
+                    Toast.makeText(MainActivity.this, "對了", Toast.LENGTH_SHORT).show();
                 }
+
+//                if (n < r) {
+//                    new AlertDialog.Builder(MainActivity.this)
+//                            .setMessage("大一點")
+//                            .show();
+//                }
+//                if (n > r) {
+//                    new AlertDialog.Builder(MainActivity.this)
+//                            .setMessage("小一點")
+//                            .show();
+//                }
+//                if (n == r) {
+//                    new AlertDialog.Builder(MainActivity.this)
+//                            .setMessage("猜對了")
+//                            .show();
+//                }
             }
             });
         }}
